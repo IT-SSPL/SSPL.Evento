43a707432a64d729aaec9ca50afc79a6d1774c13
miloszwierucki <milosz@wierucki.com>
2024-02-06 15:59:33 +0100

    Move props from metadata to viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
+import type { Viewport } from "next";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -8,9 +10,17 @@ export const metadata: Metadata = {
     "TripApp PŁ to aplikacja PWA stworzona specjalnie dla uczestników wyjazdów organizowanych przez Samorządu Studenckiego Politechniki Łódzkiej, takich jak Adapciak czy Zimowiska. Znajdziesz w niej kompleksowe informacje dotyczące kadry, harmonogramu wycieczki oraz kanał komunikacyjny, zapewniający sprawną wymianę informacji.",
   generator: "Next.js",
   manifest: "/manifest.json",
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#fff" },
+    { media: "(prefers-color-scheme: dark)", color: "#fff" },
+  ],
 };
 
 export default function RootLayout({
