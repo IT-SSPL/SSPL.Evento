fec3305794a2e5dc167b34800e23a806e73f605f
miloszwierucki <milosz@wierucki.com>
2024-02-06 16:37:01 +0100

    Hotfix: Change src icons in manifest file

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,25 +7,25 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "Aplikacja dla uczestników zimowiska",
     icons: [
       {
-        src: "/android-chrome-192x192.png",
+        src: "/icons/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/android-chrome-512x512.png",
+        src: "/icons/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/apple-touch-icon.png",
+        src: "/icons/apple-touch-icon.png",
         sizes: "180x180",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/maskable_icon.png",
+        src: "/icons/maskable_icon.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
