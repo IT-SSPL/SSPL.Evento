860c4552e2f8b054d06d3c09067e8dd307e9cfbb
miloszwierucki <milosz@wierucki.com>
2024-02-10 00:18:21 +0100

    Next part profile page

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/app/components/Header";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+import Link from "next/link";
 import React from "react";
 
 const ProfilePage = async ({ params }: { params: { id: string } }) => {
@@ -22,10 +23,10 @@ const ProfilePage = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div className="container mx-auto flex h-screen flex-col items-center">
-      <Header title={`Profil użytkownika`} />
+      <Header title={`Profil użytkownika`} isReturn />
 
       <main className="animate-in flex-1 flex flex-col w-full">
-        <div className="join py-4">
+        <div className="join pt-4 pb-6">
           <div className="avatar join-item">
             <div className="w-40 rounded-xl">
               <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -39,9 +40,33 @@ const ProfilePage = async ({ params }: { params: { id: string } }) => {
         </div>
         <div className="">
           {userData.email && (
-            <div className="join-item flex items-center p-4">
-              <p className="font-bold text-lg">Email:</p>
-              <p className="ml-2">{userData.email}</p>
+            <>
+              <a
+                className="join-item flex items-baseline"
+                href={`mailto:${userData.email}`}
+              >
+                <p className="font-bold text-lg">Email:</p>
+                <p className="ml-2">{userData.email}</p>
+              </a>
+              <div className="divider m-0.5" />
+            </>
+          )}
+          {userData.phone && (
+            <>
+              <a
+                className="join-item flex items-baseline"
+                href={`tel:+48${userData.phone}`}
+              >
+                <p className="font-bold text-lg">Telefon:</p>
+                <a className="ml-2">{userData.phone}</a>
+              </a>
+              <div className="divider m-0.5" />
+            </>
+          )}
+          {userData.description && (
+            <div className="join-item flex items-baseline">
+              <p className="font-bold text-lg">Opis:</p>
+              <p className="ml-2">{userData.description}</p>
             </div>
           )}
         </div>
